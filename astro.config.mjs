c0c3cbc5695fd21ffb1ad11573e93a0e903b3771
Aniket-508 <pawaraniket508@gmail.com>
2026-07-07 00:49:30 +0530

    feat: enhance configuration with Vercel analytics, add font support, and refactor head component for improved SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,23 @@
 import react from "@astrojs/react";
 import vercel from "@astrojs/vercel";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: vercel(),
+  adapter: vercel({
+    webAnalytics: {
+      enabled: false,
+    },
+  }),
+  fonts: [
+    {
+      cssVariable: "--font-sans",
+      fallbacks: ["sans-serif"],
+      name: "Geist Sans",
+      provider: fontProviders.fontsource(),
+    },
+  ],
   integrations: [react()],
   output: "server",
   vite: {
